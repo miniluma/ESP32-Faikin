ed22a6a884c42aeec40d310b4bfbc6405f207df0
Michael <m5idesigns.official@gmail.com>
2024-02-09 11:35:36 +0100

    Fix round to 0.5

--- ESP32/main/Faikin.c
+++ ESP32/main/Faikin.c
@@ -3251,7 +3251,7 @@ app_main ()
                         float offset = 0;
                         if(measured_temp < autot) // between min and desired temp
                         {
-                           offset = 2 - roundf(1.5*(measured_temp-min)/autot);
+                           offset = 2 - roundf((1.5*(measured_temp-min)/autot)/5)*5;
                         }
                         else // above desired temp
                         {
